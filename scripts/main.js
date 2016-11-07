3f59dd1900d2c8982df850816adac28575582d5b
andycheng <ccjeng@gmail.com>
2016-11-07 13:33:10 +0800

    no message

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("成为全宇宙最屌的前端工程师!!!"),$("#fullpage").fullpage({navigation:!0,navigationPosition:"right"})});
+$(document).ready(function(){console.log("-- Keyboard Farmer -- "),$("#fullpage").fullpage({navigation:!0,navigationPosition:"right"})});
